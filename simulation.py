4d32065d036c481a2ecda88352688c4f15101252
yourworg <69506226+yourworg@users.noreply.github.com>
2026-09-24 20:27:08 +0200

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -5,46 +5,53 @@ Created on Mon Sep  7 15:20:01 2026
 @author: oscar
 """
 
+
 #libs
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
+class Simulation:
 
-#data
-frames = 1000
-dt = 0.01
-n = 500
-epsilon = 0.1
-G = 0.001 #units m^3 kg^-1 s^-2 #6.6743 *10 ** -11 #units m^3 kg^-1 s^-2
+  def __init__(
+      self, n=500, frames=10000, dt=0.01, epsilon=0.1, G=0.001,collision_distance_sq=0.01
+  ):
+    # Parameters
+    self.n = n
+    self.frames = frames
+    self.dt = dt
+    self.epsilon = epsilon
+    self.G = G
 
+    # Initialize arrays as instance variables
+    self.pos_arr = np.random.rand(self.n, 3)
+    self.vel_arr = np.zeros((self.n, 3))
+    self.acc_arr = np.empty((self.n, 3))
+    self.mass_arr = np.ones(self.n)
 
-pos_arr = np.random.rand(n, 3)
-vel_arr = np.zeros((n, 3))
-acc_arr = np.empty((n, 3))
-mass_arr = np.ones(n)
+    self.animation_pos_matrix = []
+    self.collision_distance_sq = collision_distance_sq
 
-animation_pos_matrix = []
+  def next_frame(self):
+    # Difference matrix between points (using self. for instance variables)
+    r_diff = (
+        self.pos_arr[np.newaxis, :, :] - self.pos_arr[:, np.newaxis, :]
+    )
 
-
-#text
-
-#initialization
-
-#update loop 
-for frame in range(frames):
-    animation_pos_matrix.append(pos_arr)
-    #difference matrix between points
-    r_diff = pos_arr[np.newaxis, :, :]- pos_arr[:, np.newaxis, :]
-    
-    dist_sq = np.sum(r_diff**2, axis=-1) + epsilon**2
+    dist_sq = np.sum(r_diff**2, axis=-1) + self.epsilon**2
     np.fill_diagonal(dist_sq, np.inf)
-    inv_dist_cube = dist_sq**(-1.5)
-    term = mass_arr[np.newaxis, :, np.newaxis] * r_diff * inv_dist_cube[:, :, np.newaxis]
-    acc_arr = G * np.sum(term, axis=1)
-    vel_arr += acc_arr*dt
-    pos_arr += vel_arr*dt
-    pos_arr = pos_arr % 1  
+    inv_dist_cube = dist_sq**-1.5
+
+    term = (
+        self.mass_arr[np.newaxis, :, np.newaxis]
+        * r_diff
+        * inv_dist_cube[:, :, np.newaxis]
+    )
+    self.acc_arr = self.G * np.sum(term, axis=1)
+    self.vel_arr += self.acc_arr * self.dt
+    self.pos_arr += self.vel_arr * self.dt
+    self.pos_arr = self.pos_arr % 1
+    return self.pos_arr
     
     
         
